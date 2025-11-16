a4016a885a117fe870f8d01fc132371aa8f9f458
mikufan2000 <catlover10132000@proton.me>
2025-11-15 16:25:06 -0800

    Update math.py

--- math.py
+++ math.py
@@ -1,8 +1,38 @@
-# Math
+import math
+
+# Integer x = 1
+# Float x = 1.1
+# Complex Numbers x + 2j = a + bi
+
+# Addition
 print(10 + 3)
+
+# Subtraction
 print(10 - 3)
+
+# Multiplication
 print(10 * 3)
+
+# Floating Division
 print(10 / 3)
+
+# Integer Division
 print(10 // 3)
+
+# Modulus (Remainder of division)
 print(10 % 3)
+
+# Exponent
 print(10 ** 3)
+
+x = 10
+x = x + 3
+x += 3
+
+# Rounding
+print(round(2.9))
+
+# Absolute value
+print(abs(-2.9))
+
+print(math.ceil(2.2))
